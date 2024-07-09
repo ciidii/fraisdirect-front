58dc3b8acbcdb61553eb2d2f378ac2b5901264a5
ciidii <booba167967@gmail.com>
2024-07-09 19:38:17 +0000

    base config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Router, RouterModule, Routes} from '@angular/router';
+import {NgModule, NgModuleRef} from "@angular/core";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:HomeComponent}
+];
+@NgModule({
+imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+export class AppRoutes{
+
+}
