335d86dd430173d42986fe3c35314d9d2e2d148c
ciidii <booba167967@gmail.com>
2024-07-12 15:31:00 +0000

    spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./core/header/header.component";
+import {SpinnerComponent} from "./core/spinner/spinner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
 import {ToastrModule} from "ngx-toastr";
-
+import {SpinnerComponent} from "./spinner/spinner.component";
+import {NgxSpinnerModule} from "ngx-spinner";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxSpinnerModule
   ],
   exports:[
     CommonModule,
-    ToastrModule
+    ToastrModule,
   ]
 })
 export class CoreModule { }
