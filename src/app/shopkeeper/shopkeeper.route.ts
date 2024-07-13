598a5055c48210119ed9bd999b06393bbdf7e411
KhadijaDiasse <diassekhadidiatou14@gmail.com>
2024-07-13 10:36:04 +0000

    consulter mise a jour

--- src/app/shopkeeper/shopkeeper.route.ts
+++ src/app/shopkeeper/shopkeeper.route.ts
@@ -1,8 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AddProductComponent} from "./add.product/add.product.component";
+import {DetailProductComponent} from "./detail.product/detail.product.component";
+import {NoservicesComponent} from "./noservices/noservices.component";
 const routesKeeper:Routes=[
-  {path:"product",component:AddProductComponent}
+  {path:"product",component:AddProductComponent},
+  {path:"detailproduct",component:DetailProductComponent},
+  {path:"noservices",component:NoservicesComponent}
+
 ];
 
 @NgModule({
