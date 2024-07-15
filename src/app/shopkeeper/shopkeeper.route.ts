2ed55e1abdb47a94a15dce7a900d8925250d17d5
KhadijaDiasse <diassekhadidiatou14@gmail.com>
2024-07-15 09:54:47 +0000

    Revue

--- src/app/shopkeeper/shopkeeper.route.ts
+++ src/app/shopkeeper/shopkeeper.route.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {AddProductComponent} from "./add.product/add.product.component";
 import {DetailProductComponent} from "./detail.product/detail.product.component";
 import {NoservicesComponent} from "./noservices/noservices.component";
+import {ContactComponent} from "./contact/contact.component";
 const routesKeeper:Routes=[
   {path:"product",component:AddProductComponent},
   {path:"detailproduct",component:DetailProductComponent},
-  {path:"noservices",component:NoservicesComponent}
+  {path:"noservices",component:NoservicesComponent},
+  {path:"contact",component:ContactComponent}
 
 ];
 
