a82679133eb2b4cb9446221ad0f1a4d5a08bc84e
KhadijaDiasse <diassekhadidiatou14@gmail.com>
2024-07-16 16:48:44 +0000

    Rearrangement du Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,17 @@ import {SpinnerComponent} from "./core/spinner/spinner.component";
 import {NgxSpinnerComponent} from "ngx-spinner";
 import {CommonModule} from "@angular/common";
 import {FooterComponent} from "./core/footer/footer.component";
-
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SpinnerComponent,CommonModule,
-    NgxSpinnerComponent,
-    FooterComponent ],
+  imports: [RouterOutlet, HeaderComponent, SpinnerComponent, CommonModule, NgxSpinnerComponent, FooterComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'fraisdirect-frontend';
   constructor(private router: Router) {
-
   }
-
   ngOnInit(): void {
     this.router.navigateByUrl("/customer")
   }
